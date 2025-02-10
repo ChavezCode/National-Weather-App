e47c63d922cef0a050a43d3f2102d1e35a90f062
ChavezCode <jch1248@wgu.edu>
2025-02-09 18:19:02 -0800

    Making edits to links

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, HeaderComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
